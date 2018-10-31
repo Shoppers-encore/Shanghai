d7b169cf2f44dd9a0d844bdb5ce19005a0f343d4
wonhyeongJEONG <44572095+wonhyeongJEONG@users.noreply.github.com>
2018-10-31 20:30:30 +0900

    reviewList Data
    리뷰리스트 완성?

--- src/db/BoardDao.java
+++ src/db/BoardDao.java
@@ -1,8 +1,19 @@
 package db;
 
+import java.util.List;
+import java.util.Map;
+
 import org.apache.ibatis.session.SqlSession;
 
 import bean.SqlMapClient;
+import databean.ReviewDataBean;
 public class BoardDao {
 	private SqlSession session = SqlMapClient.getSession();
+	
+	public int getReviewCount() {
+		return session.selectOne( "User.getReviewCount" );
+	}
+	public List<ReviewDataBean> getReviewList(Map<String, String> needData){
+		return session.selectList( "User.getReviewList", needData );
+	}
 }
--- src/handler/UserViewHandler.java
+++ src/handler/UserViewHandler.java
@@ -13,13 +13,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import databean.ProductDataBean;
+import databean.ReviewDataBean;
+import db.BoardDao;
 import db.ProductDao;
 import etc.HandlerHelper;
+
 
 @Controller
 public class UserViewHandler {
 	@Resource
 	private ProductDao productDao;
+	private BoardDao boardDao;
 	@RequestMapping( "/userMailCheck" )
 	public ModelAndView userMailCheck (HttpServletRequest request, HttpServletResponse response) {
 		return new ModelAndView( "user/view/userMailCheck" );
@@ -34,6 +38,13 @@ public class UserViewHandler {
 	}
 	@RequestMapping("/reviewList")
 	public ModelAndView userReviewList(HttpServletRequest request, HttpServletResponse response) {
+		int count = boardDao.getReviewCount();	
+
+		if( count > 0 ) {
+			Map<String, String> map = new HandlerHelper().makeCount( count, request );
+			List <ReviewDataBean> articles = boardDao.getReviewList( map );
+			request.setAttribute( "reviewLists", articles );
+		}
 		return new ModelAndView("user/view/reviewList");
 	}
 	@RequestMapping("/main")
