56b280a10180dd3a84d8e06fcaf11894ceb4387d
JJureuk <44635620+JJureuk@users.noreply.github.com>
2018-11-08 15:58:18 +0900

    admViewHandler 부분 conflict 제거
    finally!

--- src/db/BoardDao.java
+++ src/db/BoardDao.java
@@ -74,7 +74,7 @@ public class BoardDao {
 		return session.delete( "Admin.reviewDelete", reviewNo );
 	}
 
-	//////// 1107 added by JH
+	//////// 1107 added by JH ^^
 	public int getReviewSearchCount(Map<String,String> map) {
 		return session.selectOne("User.getReviewCount", map);
 	}
